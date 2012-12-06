20d3346df185894f83ad95a6b3f8c568932e06b9
Ilya Khokhlov <ikhokhlov89@gmail.com>
2012-12-06 18:38:29 +0400

    recipient complete

--- gpen/Penalties/CPenaltyTicketViewController.m
+++ gpen/Penalties/CPenaltyTicketViewController.m
@@ -7,14 +7,169 @@
 //
 
 #import "CPenaltyTicketViewController.h"
+#import "Penalty.h"
+#import "Recipient.h"
+#import "CPenaltyDetailCell.h"
 
 @interface CPenaltyTicketViewController ()
-
+{
+    NSArray *m_dataSource;
+}
+@property (nonatomic, strong) NSArray *dataSource;
 @end
 
 @implementation CPenaltyTicketViewController
 
 @synthesize penalty = _penalty;
 //доставай данные по квитанции через свойство _penalty.recipient
+
+- (void) viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.dataSource = [NSArray arrayWithObjects:
+                       [NSArray arrayWithObjects:@"Протокол:", [NSString stringWithFormat:@"№ %@", [self spacedProtocolString: self.penalty.reportId]], nil],
+                       [NSArray arrayWithObjects:@"Код администратора:", [self spacedMoneyString:self.penalty.recipient.administratorCode], nil],
+                       [NSArray arrayWithObjects:@"Сумма платежа:", [NSString stringWithFormat:@"%@ рублей", [self spacedMoneyString: [NSString stringWithFormat:@"%@", self.penalty.price]]], nil],
+                       [NSArray arrayWithObjects:@"Получатель:", [self noneNilString:self.penalty.recipient.account], nil],
+                       [NSArray arrayWithObjects:@"Счет:", [self spacedCheckString: [NSString stringWithFormat:@"%@", self.penalty.uid]], nil],
+                       [NSArray arrayWithObjects:@"Инн:", [self spacedCheckString: self.penalty.recipient.inn], nil],
+                       [NSArray arrayWithObjects:@"КПП:", [self spacedCheckString: self.penalty.recipient.kpp], nil],
+                       [NSArray arrayWithObjects:@"ОКАТО:", [self spacedCheckString: self.penalty.recipient.okato], nil],
+                       [NSArray arrayWithObjects:@"КБК:", [self spacedCheckString: self.penalty.recipient.kbk], nil],
+                       [NSArray arrayWithObjects:@"Банк:", [self noneNilString:self.penalty.recipient.bank], nil],
+                       [NSArray arrayWithObjects:@"Наименование платежа:", [self noneNilString:self.penalty.recipient.billTitle], nil],
+                       nil];
+
+    [self.tableView reloadData];
+}
+
+- (NSString *) spacedProtocolString: (NSString *) protocolString
+{
+    if (!protocolString) {
+        return @"";
+    }
+    NSMutableString *temp = [NSMutableString stringWithString:protocolString];
+    
+    [temp insertString:@" " atIndex:7];
+    [temp insertString:@" " atIndex:4];
+    [temp insertString:@" " atIndex:2];
+    
+    return temp;
+}
+
+- (NSString *) spacedMoneyString: (NSString *) moneyString
+{
+    if (!moneyString) {
+        return @"";
+    }
+    NSMutableString *temp = [NSMutableString stringWithString:moneyString];
+    
+    int strLen = [temp length];
+    while (strLen >= 4)
+    {
+        [temp insertString:@" " atIndex:strLen - 3];
+        strLen -= 3;
+    }
+    
+    return temp;
+}
+
+- (NSString *) spacedCheckString: (NSString *) moneyString
+{
+    if (!moneyString) {
+        return @"";
+    }
+    NSMutableString *temp = [NSMutableString stringWithString:moneyString];
+    
+    int strLen = [temp length];
+    while (strLen >= 5)
+    {
+        [temp insertString:@" " atIndex:strLen - 4];
+        strLen -= 4;
+    }
+    
+    return temp;
+}
+
+- (NSString *) noneNilString: (NSString *) aString
+{
+    return (aString ? aString : @"");
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return (self.dataSource ? [self.dataSource count] : 0);
+}
+
+- (CGFloat) tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    CGFloat result = [self heightForVerticalText:[[self.dataSource objectAtIndex:indexPath.row] objectAtIndex:1] title:[[self.dataSource objectAtIndex:indexPath.row] objectAtIndex:0]];
+    
+    return (result < 44.0) ? 44.0 : result;
+}
+
+- (CGFloat) heightForVerticalText: (NSString *) aText title: (NSString *) aTitle
+{
+    CGFloat outerWidth = 296.0;
+    if (UIInterfaceOrientationIsLandscape(self.interfaceOrientation)) {
+        outerWidth = 436.0;
+    }
+    // use large value to avoid scrolling
+    CGFloat maxHeight = 50000.0f;
+    CGSize constraint = CGSizeMake(outerWidth, maxHeight);
+    CGSize size = [aText
+                   sizeWithFont:[UIFont fontWithName:@"PTSans-Regular" size:16.0]
+                   constrainedToSize:constraint
+                   lineBreakMode:UILineBreakModeWordWrap];
+    CGSize titleSize = [aTitle
+                        sizeWithFont:[UIFont fontWithName:@"PTSans-Regular" size:16.0]
+                        constrainedToSize:constraint
+                        lineBreakMode:UILineBreakModeWordWrap];
+    CGFloat height = size.height + titleSize.height + 11.0 * 2 + 8.0;
+    return height;
+}
+
+- (void) didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+    [super didRotateFromInterfaceOrientation:fromInterfaceOrientation];
+    
+    [self.tableView reloadData];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *penaltyVerticalCellId = @"penaltyDetailVertical";
+    
+    CPenaltyDetailCell *cell = (CPenaltyDetailCell *)[tableView dequeueReusableCellWithIdentifier:penaltyVerticalCellId];
+    [cell.labelTitle setText:[[self.dataSource objectAtIndex:indexPath.row] objectAtIndex:0]];
+    [cell.labelSubtitle setText:[[self.dataSource objectAtIndex:indexPath.row] objectAtIndex:1]];
+    return cell;
+}
+
+- (CGFloat) tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section
+{
+    return 20.0;
+}
+
+- (UIView *) tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section
+{
+    UIView *result = [[UIView alloc] init];
+    result.backgroundColor = [UIColor clearColor];
+    return result;
+}
+
+- (CGFloat) tableView:(UITableView *)tableView heightForHeaderInSection:(NSInteger)section
+{
+    return 0.0;
+}
 
 @end
